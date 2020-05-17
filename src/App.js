ea243a73e8f792cf83a5f897df23c2853fdfe723
sameerapk <sameerakmurthy@gmail.com>
2020-05-17 18:42:28 +0530

    Planning and creating component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Cards, Chart, CountryPicker } from './components'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello App</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }

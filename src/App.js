2b8c03a51322bdb06f908869b66d827633c508b9
sameerapk <sameerakmurthy@gmail.com>
2020-05-18 07:53:58 +0530

    making api call to get the data and passing the same to the cards component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import {fetchData} from './api'
 
 class App extends Component {
+    state = {
+        data : {}
+    }
+    async componentDidMount () {
+        const fetchedData = await fetchData()
+        this.setState({data: fetchedData})
+    }
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <Cards />
                 <CountryPicker />
                 <Chart />
+                
             </div>
         )
     }
